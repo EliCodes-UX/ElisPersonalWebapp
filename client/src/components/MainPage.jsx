45b6c118ac3a2ec383cd28a65b3825d0d99b38e7
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-01 13:33:59 -0700

    changed MainPage to have text in the center

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,13 +1,15 @@
 export default function MainPage() {
   return (
-    <div className='flex'>
-      <div>
-        <p className='font-permanent-marker text-center text-2xl'>
-          Hey, Im Eli from a suburb of Pheonix, Arizona. <br /> Im currently
-          developing my skills as a Software developer
-        </p>
-      </div>
+    <>
       <div>links</div>
-    </div>
+      <div className='flex flex justify-center items-center h-screen'>
+        <div className='flex items-center justify-center bg-blue-200 ml-20'>
+          <p className='font-permanent-marker text-center text-2xl'>
+            Hey, I'm Eli from a suburb of Phoenix, Arizona. <br /> I'm currently
+            developing my skills as a Software developer
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
