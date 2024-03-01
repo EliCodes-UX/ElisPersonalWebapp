57f8ce1a90097987b07a6f9820c0d5d233fe21a8
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-01 11:55:02 -0700

    added some text to the MainPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <>
-      <div className='bg-black h-10 w-full text-blue-300'>hello world</div>
+      <MainPage />
     </>
   );
 }
---
+++ client/src/components/MainPage.jsx
@@ -0,0 +1,13 @@
+export default function MainPage() {
+  return (
+    <div>
+      <div>
+        <p>
+          Hey, Im Eli from a suburb of Pheonix, Arizona. Im currently developing
+          my skills as a Software developer
+        </p>
+      </div>
+      <div>links</div>
+    </div>
+  );
+}
