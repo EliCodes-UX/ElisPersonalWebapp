6f34f70a304ba0a74d2b5a04fe37757a7e433276
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-01 13:00:35 -0700

    more changes to the config file

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,10 +1,10 @@
 export default function MainPage() {
   return (
-    <div className='bg-black'>
+    <div className='flex'>
       <div>
-        <p className=''>
-          Hey, Im Eli from a suburb of Pheonix, Arizona. Im currently developing
-          my skills as a Software developer
+        <p className='font-permanent-marker text-center text-2xl'>
+          Hey, Im Eli from a suburb of Pheonix, Arizona. <br /> Im currently
+          developing my skills as a Software developer
         </p>
       </div>
       <div>links</div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,21 @@ export default {
   content: ['./src/**/*.jsx'],
   theme: {
     extend: {},
+    fontFamily: {
+      'permanent-marker': ['Permanent Marker', 'cursive'],
+    },
+    fontSize: {
+      'xs': '0.75rem',
+      'sm': '0.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
   },
   plugins: [],
 };
