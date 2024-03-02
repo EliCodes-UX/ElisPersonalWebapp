f6741af26e7ecf70bd8f5301a63478a7647bd387
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-01 22:51:32 -0700

    more css changes

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,17 +1,20 @@
 export default function MainPage() {
   return (
     <>
-      <div className='flex justify-between items-center sticky top-13 z-10'>
-        <div className='flex items-center '>
-          <ul>
+      <div className='flex justify-between items-center sticky top-0 bg-white z-5 h-screen w-screen'>
+        {/* Sidebar */}
+        <div className='flex items-center justify-center flex-grow mr-10'>
+          <ul className='flex flex-col items-center'>
             <li>about</li>
             <li>technologies</li>
             <li>projects</li>
             <li>contact</li>
           </ul>
         </div>
-        <div>
-          <ul>
+
+        {/* Social */}
+        <div className='flex items-center justify-center flex-grow ml-10'>
+          <ul className='flex flex-col items-center'>
             <li>discord</li>
             <li>linkedin</li>
             <li>github</li>
@@ -19,6 +22,8 @@ export default function MainPage() {
           </ul>
         </div>
       </div>
+
+      {/* Text */}
       <div className='flex justify-center items-center h-screen'>
         <div className='flex items-center justify-center bg-blue-200 ml-20'>
           <p className='font-permanent-marker text-center text-2xl'>
