fc437b818c05a18a030394d1ed229877cec083bb
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-03 22:17:04 -0700

    changed and added projects grid

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -14,11 +14,20 @@ export default function MainPage() {
           Hey, Im Eli from a suburb of Phoenix, Arizona. <br /> Im currently
           developing my skills as a Software developer
         </p>
-        <p className='text-sky-500 font-kode-mono font-bold text-2xl mt-20'>
+        <p className='text-sky-500 font-kode-mono font-bold text-2xl mt-20 mb-20'>
           Technologies that I am able to program with is <br />
           Html, Css, tailwindcss, Javascript, React, Node.js, Express, and
           MongoDB
         </p>
+        <div className='grid grid-cols-3 gap-4 bg-white h-1/3 w-1/2 items-center justify-center'>
+          {/* Each square */}
+          <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-20 w-20'></div>
+          <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-20 w-20'></div>
+          <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-20 w-20'></div>
+          <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-20 w-20'></div>
+          <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-20 w-20'></div>
+          <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-20 w-20'></div>
+        </div>
       </div>
     </>
   );
