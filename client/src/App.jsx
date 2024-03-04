6c044c0a9ca59efcf52afa0c20f627ddcac20208
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-03 21:17:32 -0700

    added background to page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,15 @@ import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <>
+    <body
+      className='bg-cover bg-center h-screen'
+      style={{
+        backgroundImage:
+          "url('https://media1.giphy.com/media/l1Et7Qw1XjvunS0Io/giphy.gif?cid=ecf05e47h986u49iwrnvvfpbrbktvv1wuu5g6cuj0k2y3aw2&ep=v1_gifs_related&rid=giphy.gif&ct=g')",
+      }}
+    >
       <MainPage />
-    </>
+    </body>
   );
 }
 
