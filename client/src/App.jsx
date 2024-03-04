34e7733dcb0eddc2fffbe9eaf1816198c579ed1c
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-03 22:30:44 -0700

    added fixed gif

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,18 @@ function App() {
     'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2NsM3NoMGszend3MnBvNW9pbjBoNjF2emF2OWVkd25zdHc4N2M0MSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RHIKETUlUINYvV7CAO/giphy.gif';
 
   return (
-    <div
-      className='bg-cover bg-center h-screen sticky'
-      style={{
-        backgroundImage: `url(${gifUrl})`,
-      }}
-    >
-      <MainPage />
+    <div className='relative'>
+      {/* Background container */}
+      <div
+        className='bg-cover bg-center h-screen fixed top-0 left-0 w-screen z-0'
+        style={{
+          backgroundImage: `url(${gifUrl})`,
+        }}
+      ></div>
+      {/* Content container */}
+      <div className='relative z-10'>
+        <MainPage />
+      </div>
     </div>
   );
 }
