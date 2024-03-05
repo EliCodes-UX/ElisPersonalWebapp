9453361823d7926944cc8b88f125447b8628fd5a
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-04 22:37:50 -0700

    added some animation to sidebar

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -60,12 +60,12 @@ export default function MainPage() {
   useEffect(() => {
     setFirstParaClassName(
       showFirstParagraph
-        ? 'text-sky-500 font-kode-mono font-bold text-2xl mt-20 mb-20 animate-fade-up animate-duration-1000 animate-ease-in'
+        ? 'text-gray-200 font-kode-mono font-bold text-2xl mt-20 mb-20 animate-fade-up animate-duration-1000 animate-ease-in'
         : 'hidden'
     );
     setSecondParaClassName(
       showSecondParagraph
-        ? 'text-sky-500 font-kode-mono font-bold text-2xl mt-20 mb-20 animate-fade-up animate-duration-1000 animate-ease-in'
+        ? 'text-gray-200 font-kode-mono font-bold text-2xl mt-20 mb-20 animate-fade-up animate-duration-1000 animate-ease-in'
         : 'hidden'
     );
     setGridClassName(
@@ -75,7 +75,7 @@ export default function MainPage() {
     );
     setContactClassName(
       showContact
-        ? 'text-sky-500 font-kode-mono font-bold text-2xl mt-20 mb-20 animate-fade-up animate-duration-1000 animate-ease-in'
+        ? 'text-gray-200 font-kode-mono font-bold text-2xl mt-20 mb-20 animate-fade-up animate-duration-1000 animate-ease-in'
         : 'hidden'
     );
   }, [showFirstParagraph, showSecondParagraph, showGrid, showContact]);
@@ -110,32 +110,52 @@ export default function MainPage() {
 
   return (
     <>
-      <div className='flex justify-start pl-2 pr-2 text-amber-300 fixed h-screen items-center bg-gray-900'>
+      <div className='flex justify-start pl-2 pr-2 text-amber-300 fixed h-screen items-center bg-gray-600'>
         <ul className='flex flex-col text-lg'>
           <li className='sideBarText'>
-            <button onClick={handleAboutClick}>About</button>
+            <button
+              onClick={handleAboutClick}
+              className='hover:animate-jump animate-once animate-duration-[800ms]'
+            >
+              About
+            </button>
           </li>
           <li className='sideBarText'>
-            <button onClick={handleTechsClick}>Techs</button>
+            <button
+              onClick={handleTechsClick}
+              className='hover:animate-jump animate-once animate-duration-[800ms]'
+            >
+              Techs
+            </button>
           </li>
           <li className='sideBarText'>
-            <button onClick={handleProjectsClick}>Projects</button>
+            <button
+              onClick={handleProjectsClick}
+              className='hover:animate-jump animate-once animate-duration-[800ms]'
+            >
+              Projects
+            </button>
           </li>
           <li className='sideBarText'>
-            <button onClick={handleContactClick}>Contact</button>
+            <button
+              onClick={handleContactClick}
+              className='hover:animate-jump animate-once animate-duration-[800ms]'
+            >
+              Contact
+            </button>
           </li>
         </ul>
       </div>
       <div className='flex flex-col text-center justify-center h-screen items-center'>
         <div className={firstParaClassName}>
-          <p>
-            Hey, Im Eli from a suburb of Phoenix, Arizona. <br /> Im currently
-            developing my skills as a Software developer
+          <p className='leading-loose'>
+            Hey, Im Eli from a suburb of Phoenix, Arizona.
+            <br /> Im currently developing my skills as a Software developer
           </p>
         </div>
         <div className={secondParaClassName}>
           <div className='flex flex-col text-center justify-center h-screen items-center'>
-            <p>
+            <p className='leading-loose'>
               Technologies that I am able to program with are <br />
               Html, Css, tailwindcss, Javascript, React, Node.js, Express, and
               MongoDB
@@ -144,22 +164,24 @@ export default function MainPage() {
         </div>
         <div className={gridClassName}>
           <div className='flex flex-col items-center justify-center h-half w-half'>
-            <h1 className='text-sky-500 text-3xl font-kode-mono font-bold mb-4'>
+            <h1 className='text-gray-200 text-3xl font-kode-mono font-bold mb-4'>
               Projects
             </h1>
-            <div className='grid grid-cols-3 gap-10 bg-sky-500 p-16 rounded-lg shadow-lg shadow-sky-300'>
-              <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-60 w-60 rounded-lg'></div>
-              <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-60 w-60 rounded-lg'></div>
-              <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-60 w-60 rounded-lg'></div>
-              <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-60 w-60 rounded-lg'></div>
-              <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-60 w-60 rounded-lg'></div>
-              <div className='aspect-w-1 aspect-h-1 bg-gray-300 h-60 w-60 rounded-lg'></div>
+            <div className='grid grid-cols-3 gap-10 bg-gray-200 p-16 rounded-lg shadow-lg shadow-gray-200'>
+              <div className='aspect-w-1 aspect-h-1 bg-gray-200 h-60 w-60 rounded-lg'></div>
+              <div className='aspect-w-1 aspect-h-1 bg-gray-200 h-60 w-60 rounded-lg'></div>
+              <div className='aspect-w-1 aspect-h-1 bg-gray-200 h-60 w-60 rounded-lg'></div>
+              <div className='aspect-w-1 aspect-h-1 bg-gray-200 h-60 w-60 rounded-lg'></div>
+              <div className='aspect-w-1 aspect-h-1 bg-gray-200 h-60 w-60 rounded-lg'></div>
+              <div className='aspect-w-1 aspect-h-1 bg-gray-200 h-60 w-60 rounded-lg'></div>
             </div>
           </div>
         </div>
         <div className={contactClassName}>
           <div className='flex flex-col text-center justify-center h-screen items-center'>
-            <p>You can contact me and we can build something great</p>
+            <p className='leading-loose'>
+              You can contact me and we can build something great
+            </p>
           </div>
         </div>
       </div>
